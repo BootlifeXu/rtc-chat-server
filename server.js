72e0bf7c76fa847d2c40999a71bac09c5506e181
Samir Gaylord <129090835+BootlifeXu@users.noreply.github.com>
2025-06-19 14:31:24 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+const path = require("path");
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+app.use(express.static(path.join(__dirname, "public")));
+
+let clients = [];
+
+wss.on("connection", (ws) => {
+  clients.push(ws);
+
+  ws.on("message", (message) => {
+    clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  ws.on("close", () => {
+    clients = clients.filter((client) => client !== ws);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(\`Server running on port \${PORT}\`);
+});
